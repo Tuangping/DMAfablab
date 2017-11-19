037ef0d43f55a18d270cb79ec379c335cd7e0a88
tuangt <Tuang@s-164-67-229-12.resnet.ucla.edu>
2017-11-18 17:52:52 -0800

    update array for video loading

--- sketch.js
+++ sketch.js
@@ -1,96 +1,103 @@
-var scene1, pic1;
+var scene =[];
+var pic1,num;
 var color, alpha, playing;
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
-  background("PINK");
-  scene1 = document.querySelector('video');
-  scene1 = createVideo(['scenes/scene1.webm', 'scenes/scene1.mp4']);
-
-  scene1.attribute('playsinline', '');
-  window.enableInlineVideo(scene1);
-  scene1.hide(); //to hide the extra video on html canvas
-  scene1.play() //to show first frame of the video
-  scene1.pause(); //to stop it from playing right away
-  pic1 = loadImage("elements/numpad.png");
-  playing = false;
-  imageMode(CENTER);
-  angleMode(DEGREES);
-  color = 255;
-  alpha = 100;
+    createCanvas(innerWidth, innerHeight);
+    background("RED");
+    textSize(30);
+    fill("BLACK");
+    noStroke();
+    text("LOADING...", innerWidth/2,innerHeight/2);
+    for(var i=1; i<2; i++){
+    console.log("video loaded: "+i);
+    scene[i] = document.querySelector('video');
+    scene[i] = createVideo(['scenes/scene'+i+'.webm', 'scenes/scene'+i+'.mp4']);
+    scene[i].attribute('playsinline', '');
+    window.enableInlineVideo(scene[i]);
+    scene[i].hide();
+}
+     //to hide the extra video on html canvas
+    scene[1].play() //to show first frame of the video
+    scene[1].pause(); //to stop it from playing right away
+    pic1 = loadImage("elements/numpad.png");
+    playing = false;
+    imageMode(CENTER);
+    angleMode(DEGREES);
+    color = 255;
+    alpha = 100;
 }
 
 function draw() {
-  // console.log()
-  noStroke();
-  push();
-  translate(windowWidth / 2, windowHeight / 2);
-  noTint();
-  rotate(90);
-  image(scene1, 0, 0);
-  pop();
+    noStroke();
+    push();
+    translate(windowWidth / 2, windowHeight / 2);
+    noTint();
+    rotate(90);
+    image(scene[1], 0, 0);
+    pop();
 
-  if (playing) {
-    tint(255, 100);
-    image(pic1, windowWidth / 2, 350,1000,750);
-  }
-  
-  fill(color, 0, 0, alpha);
-  ellipse((windowWidth / 2) - 30, (windowHeight / 2) + 30, 100, 100);
-  console.log(round(scene1.time()));
+    if (playing) {
+        tint(255, 100);
+        image(pic1, windowWidth / 2, 350,1000,750);
+    }
+
+    fill(color, 0, 0, alpha);
+    ellipse((windowWidth / 2) - 30, (windowHeight / 2) + 30, 100, 100);
+    console.log("video time: "+round(scene[1].time()));
 
 
 }
 
 function touchStarted() {
-   if (mouseX > (windowWidth / 2) - 100 && mouseX < (windowWidth / 2) + 50) {
-    if (mouseY < (windowHeight / 2) + 80 && mouseY > (windowHeight / 2) - 80) {
-      console.log("hit");
-      alpha = 0;
+    if (mouseX > (windowWidth / 2) - 100 && mouseX < (windowWidth / 2) + 50) {
+        if (mouseY < (windowHeight / 2) + 80 && mouseY > (windowHeight / 2) - 80) {
+            console.log("hit");
+            alpha = 0;
+        }
+    } else {
+        color = 255;
     }
-  } else {
-    color = 255;
-  }
 
-  if (playing) {
-    scene1.stop();
-  } else {
-    scene1.play();
-    scene1.time(4);
+    if (playing) {
+        scene[1].stop();
+    } else {
+       scene[1].play();
+        scene[1].time(4);
 
-  }
-  playing = !playing;
+    }
+    playing = !playing;
 
-  return false;
+    return false;
 }
 
 function touchMoved() {
-  // if (touchX > (windowWidth / 2) - 100 && touchX < (windowWidth / 2)+50) {
-  //   if(touchY < (windowHeight/2)+80 && touchY>(windowHeight/2)-80){
-  //   console.log("hit");
-  //      color=100;
-  //   }
-  // }
- 
+    // if (touchX > (windowWidth / 2) - 100 && touchX < (windowWidth / 2)+50) {
+    //   if(touchY < (windowHeight/2)+80 && touchY>(windowHeight/2)-80){
+    //   console.log("hit");
+    //      color=100;
+    //   }
+    // }
+
 }
 
 function mousePressed() {
-  if (mouseX > (windowWidth / 2) - 100 && mouseX < (windowWidth / 2) + 50) {
-    if (mouseY < (windowHeight / 2) + 80 && mouseY > (windowHeight / 2) - 80) {
-      console.log("hit");
-      alpha = 0;
+    if (mouseX > (windowWidth / 2) - 100 && mouseX < (windowWidth / 2) + 50) {
+        if (mouseY < (windowHeight / 2) + 80 && mouseY > (windowHeight / 2) - 80) {
+            console.log("hit");
+            alpha = 0;
+        }
+    } else {
+        color = 255;
     }
-  } else {
-    color = 255;
-  }
 
-  if (playing) {
-    scene1.stop();
-  } else {
-    scene1.play();
-    scene1.time(4);
+    if (playing) {
+        scene[1].stop();
+    } else {
+        scene[1].play();
+       scene[1].time(4);
 
-  }
-  playing = !playing;
+    }
+    playing = !playing;
 
 }
