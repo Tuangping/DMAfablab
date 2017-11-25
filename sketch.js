c1fbf579295291ec278d15a474d6a2d3a234c2eb
tuangt <Tuang@wifi-131-179-21-192.host.ucla.edu>
2017-11-24 18:09:26 -0800

    add text z

--- sketch.js
+++ sketch.js
@@ -160,6 +160,8 @@ function draw() {
             textSize(30);
             text("We have many machines and materials for you to explore. However, since you are first time here, how about making some basic cut with one of the most common machines.Let's use the table saw(: ", 80, 150, windowWidth-120, windowHeight/2);
             text("curRo= "+round(curRotation), 150, windowHeight - 100);
+            fill("YELLOW");
+            text("Ro Z = "+round(rotationZ), 300, windowHeight - 100);
          
         }
     }else if (state==2){
